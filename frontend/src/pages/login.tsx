c0258f9cb77468d0d44af464aa866855bcb3de7e
ANSH KUMAR <anshkumar1399@gmail.com>
2025-02-05 01:02:07 +0530

    ui comp

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -98,7 +98,7 @@ export default function Login() {
                     </LabelInputContainer>
 
                     <button className=" bg-gradient-to-br relative group /btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 mt-5 cursor-pointer font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] mb-3"
-                        type="submit" type="submit">
+                        type="submit">
                         Login &rarr;
                     </button>
 
